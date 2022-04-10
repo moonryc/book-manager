7264917fb6e264c1b011c0ce4fcd6b05ea96b20b
moonryc <rycmoon@gmail.com>
2022-04-09 23:01:17 -0500

    resolvers now function correctly

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -2,72 +2,100 @@
 const { User } = require('../models');
 // import sign token function from auth
 const { signToken } = require('../utils/auth');
+const {AuthenticationError} = require("apollo-server-express");
 
 module.exports = {
   // get a single user by either their id or their username
-  async getSingleUser({ user = null, params }, res) {
-    const foundUser = await User.findOne({
-      $or: [{ _id: user ? user._id : params.id }, { username: params.username }],
-    });
-
-    if (!foundUser) {
-      return res.status(400).json({ message: 'Cannot find a user with this id!' });
+  async getSingleUser(_,__,context) {
+    console.log(context.user)
+    if(!context.user){
+      throw new AuthenticationError('You are not logged in')
+    }
+    try{
+      const foundUser = await User.findOne({ _id: context.user._id });
+      if (!foundUser) {
+        throw new AuthenticationError('Cannot find a user with this id!')
+      }
+      return foundUser;
+    }catch (e) {
+      throw new AuthenticationError(JSON.stringify(e))
     }
 
-    res.json(foundUser);
   },
+
   // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
-  async createUser({ body }, res) {
-    const user = await User.create(body);
-
-    if (!user) {
-      return res.status(400).json({ message: 'Something is wrong!' });
+  async createUser(_,{user:userInput}) {
+    try{
+      const user = await User.create(userInput);
+      if (!user) {
+        throw new AuthenticationError('Something is wrong!')
+      }
+      const token = signToken(user);
+      return { token, user };
+    }catch (e) {
+      throw new AuthenticationError(e)
     }
-    const token = signToken(user);
-    res.json({ token, user });
   },
+
   // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
   // {body} is destructured req.body
-  async login({ body }, res) {
-    const user = await User.findOne({ $or: [{ username: body.username }, { email: body.email }] });
-    if (!user) {
-      return res.status(400).json({ message: "Can't find this user" });
-    }
+  async login(_, {loginInput: {username = null, email = null, password}}) {
+    console.log(username,email,password)
+    try{
+      const user = await User.findOne({ $or: [{ username }, { email }] });
+      if (!user) {
+        throw new AuthenticationError('Cannot find this user')
+      }
+      const correctPw = await user.isCorrectPassword(password);
 
-    const correctPw = await user.isCorrectPassword(body.password);
-
-    if (!correctPw) {
-      return res.status(400).json({ message: 'Wrong password!' });
+      if (!correctPw) {
+        throw new AuthenticationError('wrong password')
+      }
+      const token = signToken(user);
+      return { token, user };
+    }catch(e){
+      throw new AuthenticationError(JSON.stringify(e))
     }
-    const token = signToken(user);
-    res.json({ token, user });
   },
+
+
   // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
   // user comes from `req.user` created in the auth middleware function
-  async saveBook({ user, body }, res) {
-    console.log(user);
+  async saveBook(_,{ book }, context) {
+    if(!context.user){
+      throw new AuthenticationError('User is not logged in')
+    }
     try {
       const updatedUser = await User.findOneAndUpdate(
-        { _id: user._id },
-        { $addToSet: { savedBooks: body } },
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: book } },
         { new: true, runValidators: true }
       );
-      return res.json(updatedUser);
-    } catch (err) {
-      console.log(err);
-      return res.status(400).json(err);
+      return updatedUser;
+    } catch (e) {
+      throw new AuthenticationError(JSON.stringify(e))
+
     }
   },
+
   // remove a book from `savedBooks`
-  async deleteBook({ user, params }, res) {
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $pull: { savedBooks: { bookId: params.bookId } } },
-      { new: true }
-    );
-    if (!updatedUser) {
-      return res.status(404).json({ message: "Couldn't find user with this id!" });
+  async deleteBook(_,{bookId}, context) {
+    if(!context.user){
+      throw new AuthenticationError('User is not logged in')
     }
-    return res.json(updatedUser);
+    try{
+      const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId} } },
+          { new: true }
+      );
+      if (!updatedUser) {
+        throw new AuthenticationError('Could not find user with this id')
+      }
+      return updatedUser;
+    }catch (e) {
+      throw new AuthenticationError(JSON.stringify(e))
+    }
+
   },
 };
